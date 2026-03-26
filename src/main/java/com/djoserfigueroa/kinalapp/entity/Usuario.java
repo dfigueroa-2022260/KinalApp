c9a101545ac9b637aaff4d591381fde119693b0c
dfigueroa-2022260 <dfigueroa-2022260@kinal.edu.gt>
2026-03-25 21:35:02 -0600

    Refactor: Se agregó la entidad Usuario

--- src/main/java/com/djoserfigueroa/kinalapp/entity/Usuario.java
+++ src/main/java/com/djoserfigueroa/kinalapp/entity/Usuario.java
@@ -1,4 +1,87 @@
 package com.djoserfigueroa.kinalapp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo_usuario")
+    private int codigoUsuario;
+
+    @Column(name = "username", length = 45)
+    private String username;
+
+    @Column(name = "password", length = 45)
+    private String password;
+
+    @Column(name = "email", length = 60)
+    private String email;
+
+    @Column(name = "rol", length = 45)
+    private String rol;
+
+    @Column(name = "estado")
+    private int estado;
+
+    public Usuario() {
+    }
+
+    public Usuario(String username, String password, String email, String rol, int estado) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.rol = rol;
+        this.estado = estado;
+    }
+
+    public int getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public void setCodigoUsuario(int codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
 }
